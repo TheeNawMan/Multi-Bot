602af6fa6755c58ad659973c4e6e0a00df3f6479
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2019-06-09 14:32:51 -0500

    Update discordSrv.py

--- srv/discordSrv.py
+++ srv/discordSrv.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-import re
-import socket
-from srv.jsonLoader import HOST, CHAN, NICK, PASS, PORT, jsonCfg
-from srv.cmd import *
+import discord
+
+TOKEN = 'NTg3MzQ1NjMzNzQ5MjM3Nzk0.XP1Ynw.UPnqr8r8m-l712VgPL29OTnJxCA'
 
 def discordStart():
-    if jsonCfg["other"]["debug"] == "on":
-        print("HOST: " + HOST + "\nCHAN: " + CHAN + "\nNICK: " + NICK + "\nPASS: " + PASS)
-    else:
-        print(None)
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print("The bot is ready!")
+        await client.change_presence( activity=discord.Game(name="Making a Bot"))
+    @client.event
+    async def on_message (message):
+        if message.author == client.user:
+            return
+        if message.content.startswith('!hello'):
+            msg = 'Hello {0.author.mention}'.format(message)
+            await client.send_message(message.channel, msg)
+
+    client.run(TOKEN)
+
+discordStart()
