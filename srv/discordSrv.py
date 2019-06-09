1d717129f374f50f9e192670b97863ff757bafeb
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2019-06-09 17:06:30 -0500

    Update discordSrv.py
    # Working on getting the discord version of Senpai Bot to work. Command connects to DIscord, however, I am still unable to submit commands

--- srv/discordSrv.py
+++ srv/discordSrv.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
 
 import discord
-
+from discord.ext import commands
 
 TOKEN = 'NTg3MzQ1NjMzNzQ5MjM3Nzk0.XP1Ynw.UPnqr8r8m-l712VgPL29OTnJxCA'
 
-def discordStart():
-    client = discord.Client()
-
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+author = discord.User.id
+def startdiscord():
     @client.event
-    async def on_ready():
-        print("The bot is ready!")
-        await client.change_presence( activity=discord.Game(name="Making a Bot"))
-    @client.event
-    async def on_message (message):
+    async def on_message(message):
+        # we do not want the bot to reply to itself
         if message.author == client.user:
             return
 
         if message.content.startswith('!hello'):
-            msg = 'Hello {0.author.mention}'.format(message)
-            await 
+            msg = 'Hello this is a test'
+            await bot.on_message(msg)
+
+    @bot.command
+    async def test(ctx):
+        await ctx.send('I heard you! {0}'.format(ctx.author))
+
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
 
     client.run(TOKEN)
 
-discordStart()
+
+startdiscord()
